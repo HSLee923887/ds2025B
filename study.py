26f59f79443a34b91a6ef726a4424246e48f770c
hsLeecoder <dlgotjd2003@gmail.com>
2025-04-11 23:30:06 +0900

    O(log n) 이진 탐색(반씩 줄여나감)

--- study.py
+++ study.py
@@ -1,6 +1,20 @@
-n = int(input("정수 입력 : "))
-result = 0
-for i in range(1, n+1): #O(n)
-    result = result + i
-# result = n * (n + 1) // 2  # O(1)
-print(result)
+import random
+
+answer = random.randint(1, 100)
+
+win = False
+for i in range(7):
+    guess = int(input(f"{7-i}번의 기회가 남았습니다. 숫자 입력 : "))
+
+    if guess > answer :
+        print("입력하신 수는 정답보다 큰 수 입니다. HIGH")
+    elif guess < answer :
+        print("입력하신 수는 정답보다 작은 수 입니다. LOW")
+    else:
+        win = True
+        break
+
+if win:
+    print("정답입니다! You win!")
+else:
+    print("You lose. The answer is ", answer, ".")

9da35215de2181e0f71647a023dc1f947dbc050d
hsLeecoder <dlgotjd2003@gmail.com>
2025-04-12 22:20:59 +0900

    set(), {a, b, c} - 중복된 값을 없애주는 코드

--- study.py
+++ study.py
@@ -1,20 +1,4 @@
-def duplicate_city(cities):
-    result = list()
-    s = set()
-
-    for city in cities:
-        l1 = len(s)
-        s.add(city)
-        l2 = len(s)
-        #집합에 도시 추가하기 전과 후의 길이를 비교함. 같다면 중복된 도시라는 뜻. list에 추가함
-        if l1 == l2:
-            result.append(city)
-    return result
-
-
-cities = ['Incheon', 'Incheon', 'Incheon', 'Gimpo', 'Seoul', 'Seoul']
-#cities = {'Incheon', 'Incheon', 'Incheon', 'Gimpo', 'Seoul', 'Seoul'}
-cities.append('Anyang')
-cities.append('Seoul')
-print(cities)
-print(set(duplicate_city(cities)))
+city = ['Incheon', 'Incheon', 'Incheon', 'Gimpo', 'Seoul', 'Seoul']
+#city = {'Incheon', 'Incheon', 'Incheon', 'Gimpo', 'Seoul', 'Seoul'}
+#city = set(city)
+print(city)

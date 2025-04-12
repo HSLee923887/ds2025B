ed0cffef10f3894ecb0d55cd045ef5b6c1cd547b
hsLeecoder <dlgotjd2003@gmail.com>
2025-04-12 22:17:53 +0900

    2개의 리스트에서 중복된 값을 찾아주는 메소드

--- study.py
+++ study.py
@@ -1,20 +1,10 @@
-import random
+def intersection(l1, l2):
+    l3 = list()
+    for v in l1:
+        if v in l2:
+            l3.append(v)
+    return l3
 
-answer = random.randint(1, 100)
-
-win = False
-for i in range(7):
-    guess = int(input(f"{7-i}번의 기회가 남았습니다. 숫자 입력 : "))
-
-    if guess > answer :
-        print("입력하신 수는 정답보다 큰 수 입니다. HIGH")
-    elif guess < answer :
-        print("입력하신 수는 정답보다 작은 수 입니다. LOW")
-    else:
-        win = True
-        break
-
-if win:
-    print("정답입니다! You win!")
-else:
-    print("You lose. The answer is ", answer, ".")
+l1 = [45, 5, 22, 31, 7, 19]
+l2 = [2, 1, 5, 22, 7, 38, 27, 19, 13, 41]
+print(intersection(l1, l2))

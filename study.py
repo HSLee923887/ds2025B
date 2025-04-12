5aa6520aa8e7ed90de7ed8ed0a64befb2f9e530e
hsLeecoder <dlgotjd2003@gmail.com>
2025-04-12 22:18:55 +0900

    리스트 안의 중복된 값만 골라내는 메소드

--- study.py
+++ study.py
@@ -1,10 +1,20 @@
-def intersection(l1, l2):
-    l3 = list()
-    for v in l1:
-        if v in l2:
-            l3.append(v)
-    return l3
+def duplicate_city(cities):
+    result = list()
+    s = set()
 
-l1 = [45, 5, 22, 31, 7, 19]
-l2 = [2, 1, 5, 22, 7, 38, 27, 19, 13, 41]
-print(intersection(l1, l2))
+    for city in cities:
+        l1 = len(s)
+        s.add(city)
+        l2 = len(s)
+        #집합에 도시 추가하기 전과 후의 길이를 비교함. 같다면 중복된 도시라는 뜻. list에 추가함
+        if l1 == l2:
+            result.append(city)
+    return result
+
+
+cities = ['Incheon', 'Incheon', 'Incheon', 'Gimpo', 'Seoul', 'Seoul']
+#cities = {'Incheon', 'Incheon', 'Incheon', 'Gimpo', 'Seoul', 'Seoul'}
+cities.append('Anyang')
+cities.append('Seoul')
+print(cities)
+print(set(duplicate_city(cities)))

1d05e99be65dc029765764b6b97f68bef75fd05b
hsLeecoder <dlgotjd2003@gmail.com>
2025-04-19 02:13:05 +0900

    링크드 리스트 이해2. remove메소드 추가. 버그 수정

--- study.py
+++ study.py
@@ -18,9 +18,28 @@ class LinkedList:
             current = current.link  #처음에 노드10을 가리키니까 current는 노드10이 됨.노드10은 노드-9를 가리키고있으니 true. current는 다시 노드-9가 됨.
         current.link = Node(data)   #append할 값을 추가. 가리키게 함
 
+    def remove(self, target):
+        current = self.head                     #current가 노드8을 가리킴
+
+        if self.head is None:
+            return "리스트가 비어 있습니다"
+
+        if self.head.data == target:            #처음에 머리 data검사 지우려는 값이 머리 일 때만 true를 반환 ex) ll.remove(10) 8 != 10
+            self.head = self.head.link          #false기 때문에 실행 x
+            current.link = None                 #다음 노드와 연결을 끊음
+            return
+        previous = None                         #변수 None으로 초기화
+        while current:                          #current에 값이 있을 때
+            if current.data == target:          #8 != 10, 10 == 10
+                previous.link = current.link    #실행 안 함, previous.link가 -9를 가리킴(다음 노드 기억)
+                current.link = None             #값을 찾으면 다음으로 가는 링크만 끊음
+                break
+            previous = current                  #previous가 current(노드8)를 가리킴. previous는 link가 None인 노드10을 가리킴
+            current = current.link              #current는 노드10을 가리키게 됨(루프O). current는 None이 됨(루프 탈출)
+
     def search(self, target):
         current = self.head
-        while current.link:
+        while current:
             if current.data == target:
                 return f"{target}을(를) 찾았습니다"
             else:
@@ -45,6 +64,13 @@ ll.append(-9)
 print(ll)
 print(ll.search(100))
 print(ll.search(10))
+
 for _ in range(10):
      ll.append(random.randint(1, 30))
 print(ll.search(27))
+
+ll.remove(10)
+ll.remove(8)
+ll.remove(15)
+ll.remove(20)
+print(ll)
